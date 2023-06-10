b71cc00cb485922ff16d2026c31ea912a46b412c
ivan_ver89 <ivan_ver89@mail.ru>
2023-06-10 06:36:29 +0300

    another prepare

--- src/prepare.py
+++ src/prepare.py
@@ -23,27 +23,32 @@ random.seed(params['seed'])
 input_path = sys.argv[1]
 os.makedirs("data/prepared")
 
+
+def prepare_data(dataset):
+    std_columns = ['age', 'bmi', 'HbA1c_level', 'blood_glucose_level']
+    dataset['gender'] = dataset['gender'].apply(lambda x: 0 if x == 'Female' else 1)
+    dataset = pd.get_dummies(dataset, columns=['gender', 'hypertension', 'heart_disease', 'smoking_history'], dtype=int)
+
+    pipline_res = num_pipline = Pipeline([
+        ('StandardScaler', StandardScaler()),
+        ('MinMaxScaler', MinMaxScaler())
+        ]).fit_transform(dataset[std_columns])
+    for i, col_name in enumerate(std_columns):
+        dataset[col_name] = pipline_res[:, i]
+    return dataset
+
+
+
 output_train = os.path.join("data", "prepared", "train.csv")
 output_test = os.path.join("data", "prepared", "test.csv")
 
 df = pd.read_csv(input_path)
 
-df['gender'] = df['gender'].apply(lambda x: 0 if x == 'Female' else 1)
-df = pd.get_dummies(df, columns=['gender', 'hypertension', 'heart_disease', 'smoking_history'], dtype=int)
+test, train = train_test_split(df, train_size=params['split'], random_state=params['seed'], stratify=df['diabetes'])
 
+test = prepare_data(test)
+train = prepare_data(train)
 
-std_columns = ['age', 'bmi', 'HbA1c_level', 'blood_glucose_level']
-
-pipline_res = num_pipline = Pipeline([
-    ('StandardScaler', StandardScaler()),
-    ('MinMaxScaler', MinMaxScaler())
-]).fit_transform(df[std_columns])
-
-for i, col_name in enumerate(std_columns):
-    df[col_name] = pipline_res[:, i]
-
-
-test, train =  train_test_split(df, train_size=params['split'], random_state=params['seed'])
 
 train.to_csv(output_train, index=False)
 test.to_csv(output_test, index=False)
